a8ca4c05eabbdd6b3b371b7164dc2c3156d9174f
Abhishek <abmishra700@gmail.com>
2022-03-02 10:22:28 +0530

    aaded Header js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Header } from "./containers/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>product list </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Aveosoft</h2>
+      </div>
+    </div>
+  );
+};
